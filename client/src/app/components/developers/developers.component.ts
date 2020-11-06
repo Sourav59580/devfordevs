b680d1772806f7aad15ce7689937e7c775278c5a
sourav santra <souravsantra59580@gmail.com>
2020-11-06 18:03:29 +0530

    15th commit(06/11/2020)

--- client/src/app/components/developers/developers.component.ts
+++ client/src/app/components/developers/developers.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DeveloperService } from "../../services/developer.service";
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-developers',
@@ -6,14 +9,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./developers.component.css']
 })
 export class DevelopersComponent implements OnInit {
+  user: any;
+  developers: any;
+  followings: any;
 
-  constructor() { }
+  constructor(
+    private _DeveloperService: DeveloperService,
+    private _flashMessagesService: FlashMessagesService,
+    private _AuthService: AuthService,) { 
+      this.user = this._AuthService.getUserData();
+      this.user = JSON.parse(this.user);
+
+      this._DeveloperService.getAllDevelopers().subscribe(res => {
+        console.log(res.response)
+        this.developers = res.response
+      }, err => {
+        console.log(err);
+        return false;
+      })
+
+      this._DeveloperService.getAllFollowings(this.user.id).subscribe(res => {
+        console.log(res.response)
+        this.followings = res.response
+        console.log(this.followings)
+      }, err => {
+        console.log(err);
+        return false;
+      })
+
+    }
 
   ngOnInit(): void {
   }
 
-  follow(followingId){
-    console.log(followingId);
+  checkDeveloper(id){
+    if(id==this.user.id)
+    return false;
+
+    for(var i=0;i<this.followings.length;i++)
+    {
+      if(id==this.followings[i])
+      return false;
+    }
+
+    return true;
+  }
+
+  follow(e,followingId){
+    const id = e.target.id;
+    const html = document.getElementById(id).innerHTML
+   
+    const data = {
+      ownId : this.user.id,
+      followingId: followingId
+    }
+    console.log(data)
+    if(html=="Follow")
+    {
+      document.getElementById(id).innerHTML= "<span class='spinner-grow spinner-grow-sm' role='status' aria-hidden='true'></span>Loading...";
+      this._DeveloperService.following(data).subscribe(res=>{
+        if(res.success)
+        document.getElementById(id).innerHTML = "Unfollow";
+        else
+        {
+          document.getElementById(id).innerHTML = "Follow";
+          this._flashMessagesService.show(res.msg, { cssClass: 'alert-danger' });
+        }
+      })
+    }else{
+      document.getElementById(id).innerHTML= "<span class='spinner-grow spinner-grow-sm' role='status' aria-hidden='true'></span>Loading...";
+      this._DeveloperService.unfollowing(data).subscribe(res=>{
+        if(res.success)
+        document.getElementById(id).innerHTML = "Follow";
+        else
+        {
+          document.getElementById(id).innerHTML = "Unfollow";
+          this._flashMessagesService.show(res.msg, { cssClass: 'alert-danger' });
+        }
+      })
+    }
+   
   }
 
 }
--- routes/developer.js
+++ routes/developer.js
@@ -3,6 +3,40 @@ const router = express.Router()
 const config = require("../config/database");
 const User = require("../models/user");
 
+//@type    GET
+//$route   /developers/getAll
+//$desc    router for getting All Developers.
+//@access  private
+router.get("/getAll",(req,res)=>{
+    User.find()
+    .then(result =>{
+        console.log(result);
+        res.json({response:result});
+    })
+    .catch(err => {
+        console.log(err);
+        res.json({success:false,msg: "Something went wrong"});
+    })
+})
+
+//@type    POST
+//$route   /developers/getAllFollowings
+//$desc    router for getting All Developers.
+//@access  private
+router.post("/getAllFollowings",(req,res)=>{
+    const id = req.body.id;
+    User.findById(id)
+    .then(result =>{
+        console.log(result.following);
+        res.json({response:result.following});
+    })
+    .catch(err => {
+        console.log(err);
+        res.json({success:false,msg: "Something went wrong"});
+    })
+})
+
+
 
 //@type    POST
 //$route   /developers/follow
@@ -13,45 +47,96 @@ router.post("/follow",(req,res)=>{
     const ownId = req.body.ownId;
 
 
-    // User.findByIdAndUpdate(ownId,{
-    //     $pull: {
-    //         following: followingId
-    //     }
-    // }, {
-    //     upsert: true
-    // })
-    // .then((user) => {
-    //     User.findByIdAndUpdate(followingId,{
-    //         $pull: {
-    //             followers: ownId
-    //         }
-    //     }, {
-    //         upsert: true
-    //     })
-    //     .then((user) => {
-    //         console.log(user);
-    //         res.json({
-    //             success: true,
-    //             msg: 'Success.'
-    //         });
+    User.findByIdAndUpdate(ownId,{
+        $push: {
+            following: followingId
+        }
+    }, {
+        upsert: true
+    })
+    .then((user) => {
+        User.findByIdAndUpdate(followingId,{
+            $push: {
+                followers: ownId
+            }
+        }, {
+            upsert: true
+        })
+        .then((user) => {
+            console.log(user);
+            res.json({
+                success: true,
+                msg: 'Success.'
+            });
 
-    //     })
-    //     .catch((err) => {
-    //         console.log(err);
-    //         res.json({
-    //             success: false,
-    //             msg: 'Something went wrong.'
-    //         });
-    //     })
+        })
+        .catch((err) => {
+            console.log(err);
+            res.json({
+                success: false,
+                msg: 'Something went wrong.'
+            });
+        })
 
-    // })
-    // .catch((err) => {
-    //     console.log(err);
-    //     res.json({
-    //         success: false,
-    //         msg: 'Something went wrong.'
-    //     });
-    // })
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({
+            success: false,
+            msg: 'Something went wrong.'
+        });
+    })
+})
+
+
+//@type    POST
+//$route   /developers/followRemove
+//$desc    router for adding new question.
+//@access  private
+router.post("/followRemove",(req,res)=>{
+    const followingId = req.body.followingId;
+    const ownId = req.body.ownId;
+
+
+    User.findByIdAndUpdate(ownId,{
+        $pull: {
+            following: followingId
+        }
+    }, {
+        upsert: true
+    })
+    .then((user) => {
+        User.findByIdAndUpdate(followingId,{
+            $pull: {
+                followers: ownId
+            }
+        }, {
+            upsert: true
+        })
+        .then((user) => {
+            console.log(user);
+            res.json({
+                success: true,
+                msg: 'Success.'
+            });
+
+        })
+        .catch((err) => {
+            console.log(err);
+            res.json({
+                success: false,
+                msg: 'Something went wrong.'
+            });
+        })
+
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json({
+            success: false,
+            msg: 'Something went wrong.'
+        });
+    })
 })
 
 
